c61800019b694a9db2449464fb497e6a46be93bd
Zeyad Zidan <iamzeyadzidan@gmail.com>
2023-05-22 20:53:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@
 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 #define AST_NUM 10 // Number of asteroids.
 
